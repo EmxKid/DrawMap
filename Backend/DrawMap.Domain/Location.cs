e3293e1276f78f281cb4712c7803c4a80bae9285
Sobol Mikhail <sobolm58@gmail.com>
2026-03-19 03:32:03 +0700

    fix back

--- Backend/DrawMap.Domain/Location.cs
+++ Backend/DrawMap.Domain/Location.cs
@@ -5,6 +5,7 @@ public class Location
     public string? Id { get; set; }
     public double Longitude { get; set; }
     public double Latitude { get; set; }
+    public string RouteId { get; set; }
     public DateTime Timestamp { get; set; }
     public Photo? Photo { get; set; }
 }
